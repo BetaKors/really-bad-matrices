1ccbf9d07d19bb6e1de128b13b904ba8a68b0b83
BetaKors <leparkorsz@gmail.com>
2022-06-05 15:39:29 -0300

    cleanup + cls / clear command + exit command fix

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from matrices import Matrix
+from os import system
 
 
-def parse_operation(operation: str) -> str:
-    operation = f'({operation})'
-    return operation.replace('{', '[').replace('}', ']').replace('[[', 'Matrix([[').replace(']]', ']])').replace('=', ':=')
+def clear():
+    system('cls || clear')
 
 
-operation = ''
+clear()
 
-while operation != 'exit':
-    print(eval(operation := parse_operation(input('> ')), globals(), locals()))
+print('=' * 10 + ' Very, very bad matrix operations, by BetaKors ' + '=' * 10)
+
+while True:
+    i = input('> ')
+
+    if i == 'cls' or i == 'clear':
+        clear()
+        continue
+
+    if i == 'exit':
+        break
+
+    # converts a = {{1, 2}, {3, 4}} to (a := Matrix([[1, 2], [3, 4]]))
+    operation = f'({operation})' \
+                .replace('{', '[') \
+                .replace('}', ']') \
+                .replace('[[', 'Matrix([[') \
+                .replace(']]', ']])') \
+                .replace('=', ':=')
+
+    result = eval(operation, globals(), locals())  ## yes i know this is very very very very very unsafe
+
+    if result is not None:
+        print(result)
